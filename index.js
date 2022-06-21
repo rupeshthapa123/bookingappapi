116d2d4ddad648c8728b8a98600f7cde988ab56a
Rupesh <rupesh.thapa2050@gmail.com>
2022-06-21 13:18:36 +0545

    update

--- index.js
+++ index.js
@@ -17,7 +17,13 @@ dotenv.config();
  */
 const connect = async () =>{
     try {
-        await mongoose.connect(process.env.MONGO);
+        await mongoose.connect(process.env.MONGO, {
+            useFindAndModify: false,
+            useUnifiedTopology: true,
+            useNewUrlParser: true,
+            useCreateIndex: true,
+            server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },
+        },);
         console.log("Connected to mongoDB.");
     }catch (error){
         throw error;
